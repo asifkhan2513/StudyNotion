a8a5682892bc49a68b2c7a01975ae3d0773a5e69
Mo Asif <141583278+asifkhan2513@users.noreply.github.com>
2024-05-17 09:38:22 +0400

    processing

---
+++ server/config/database.js
@@ -0,0 +1,29 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// exports.connect = () => {
+//     mongoose.connect(process.env.MONGODB_URL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology:true,
+//     })
+//     .then(() => console.log("DB Connected Successfully"))
+//     .catch( (error) => {
+//         console.log("DB Connection Failed");
+//         console.error(error);
+//         process.exit(1);
+//     } )
+// };
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Db connection successful");
+  } catch (error) {
+    console.log("Issue in connectivity");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
